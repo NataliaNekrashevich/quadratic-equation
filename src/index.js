6109431851e0946eb135f8a07f0f02143604a73d
Nekrashevich, Natalya <n.nekrashevich@a1qa.com>
2018-09-18 15:35:57 +0300

    100 tests pass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let coefficients = equation.split(' ').join('').split(/\*x\^2|\*x/);
+  let sqD = Math.sqrt(coefficients[1] * coefficients[1] - 4 * coefficients[0] * coefficients[2]);
+  let solution = [];
+  solution[0] = Math.round((-coefficients[1] - sqD) / (2 * coefficients[0]));
+  solution[1] = Math.round((-coefficients[1] + sqD) / (2 * coefficients[0]));
+  return solution;
 }
